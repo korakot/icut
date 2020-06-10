2c18c4bbfbd6dac711171f79182f53b752bbae1e
Korakot Chaovavanich <korakot@MBP-kora.local>
2020-06-10 16:18:12 +0700

    upload 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="icut", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Korakot Chaovavanich",
     author_email="korakot@gmail.com",
     description="A fast Thai tokenization library",
